564602f7b43fa87682739b02fc0681d5fd8d92d7
Pierre Guillen <hi@cerbero.dev>
2023-09-21 04:59:40 -0500

    Create package wirh models of weather_api

---
+++ packages/weather_api/lib/src/models/weather.dart
@@ -0,0 +1,9 @@
+class Weather {
+  const Weather({
+    required this.temperature,
+    required this.weatherCode,
+  });
+
+  final double temperature;
+  final double weatherCode;
+}
---
+++ packages/weather_api/lib/weather_api.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library weather_api;
+
+export 'src/models/models.dart';
