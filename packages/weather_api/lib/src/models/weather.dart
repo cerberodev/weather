f5fdcb3aa15af365ed25fb47bab35e7f0c115739
Pierre Guillen <hi@cerbero.dev>
2023-09-21 05:35:54 -0500

    Add json_annotation and Documentation

--- packages/weather_api/lib/src/models/weather.dart
+++ packages/weather_api/lib/src/models/weather.dart
@@ -1,9 +1,28 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'weather.g.dart';
+
+@JsonSerializable()
+
+/// {@template Weather}
+/// Weather model represents the Weather of the city search.
+/// {@endtemplate}
 class Weather {
+  /// {@macro Weather}
   const Weather({
     required this.temperature,
     required this.weatherCode,
   });
 
+  /// The Weather factory.
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
+
+  /// The Weather [temperature].
   final double temperature;
+
+  @JsonKey(name: 'weathercode')
+
+  /// The Weather [weatherCode].
   final double weatherCode;
 }
