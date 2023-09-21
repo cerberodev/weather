a3734cdb01ee53a397960d6329b1ef3aaae371c9
Pierre Guillen <hi@cerbero.dev>
2023-09-21 05:35:48 -0500

    Add json_annotation and Documentation

--- packages/weather_api/lib/src/models/location.dart
+++ packages/weather_api/lib/src/models/location.dart
@@ -1,4 +1,14 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'location.g.dart';
+
+@JsonSerializable()
+
+/// {@template Location}
+/// Location model represents the Location of the city search.
+/// {@endtemplate}
 class Location {
+  /// {@macro Location}
   const Location({
     required this.id,
     required this.name,
@@ -6,8 +16,19 @@ class Location {
     required this.longitude,
   });
 
+  /// The Location factory.
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
+
+  /// The Location [id].
   final int id;
+
+  /// The Location [name].
   final String name;
+
+  /// The Location [latitude].
   final double latitude;
+
+  /// The Location [longitude].
   final double longitude;
 }
